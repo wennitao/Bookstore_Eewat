f5e11fe8d0427f48cdcb1eef5945876189708b8a
wennitao <wennitao@126.com>
2023-04-16 15:56:06 +0800

    seller
    Former-commit-id: 6f1d86ee31947d602747e94af29b089fbf09fc72

--- bookstore/be/model/database.py
+++ bookstore/be/model/database.py
@@ -20,3 +20,36 @@ def init_database():
 def getDatabase () -> pymongo.database.Database:
     global database_instance
     return database_instance.getDatabase()
+
+def user_id_exist(user_id) -> bool:
+    user_collection = getDatabase()['user']
+    # cursor = self.conn.execute("SELECT user_id FROM user WHERE user_id = ?;", (user_id,))
+    # row = cursor.fetchone()
+    result = user_collection.find ({'user_id': user_id})
+    result_list = list (result)
+    if len (result_list) == 0:
+        return False
+    else:
+        return True
+    
+def book_id_exist(store_id, book_id):
+    # cursor = self.conn.execute("SELECT book_id FROM store WHERE store_id = ? AND book_id = ?;", (store_id, book_id))
+    # row = cursor.fetchone()
+    store_collection = getDatabase()['store']
+    result = store_collection.find ({'store_id': store_id, 'book_id': book_id})
+    result_list = list (result)
+    if len (result_list) == 0:
+        return False
+    else:
+        return True
+
+def store_id_exist(store_id):
+    # cursor = self.conn.execute("SELECT store_id FROM user_store WHERE store_id = ?;", (store_id,))
+    # row = cursor.fetchone()
+    user_store_collection = getDatabase()['user_store']
+    result = user_store_collection.find ({'store_id': store_id})
+    result_list = list (result)
+    if len (result_list) == 0:
+        return False
+    else:
+        return True
--- bookstore/be/model/seller.py
+++ bookstore/be/model/seller.py
@@ -1,26 +1,43 @@
-import sqlite3 as sqlite
+# import sqlite3 as sqlite
+from typing import Tuple
+import pymongo
+import pymongo.errors
 from be.model import error
-from be.model import db_conn
+# from be.model import db_conn
+from be.model.collection import Collection
+from be.model.database import user_id_exist, book_id_exist, store_id_exist
 
-
-class Seller(db_conn.DBConn):
+class Seller():
 
     def __init__(self):
-        db_conn.DBConn.__init__(self)
+        # db_conn.DBConn.__init__(self)
+        self.storeCollection = Collection("store").collection
+        self.storeCollection.create_index([("store_id", 1), ("book_id", 1)], unique=True)
+        self.userStoreCollection = Collection ("user_store").collection
+        self.userStoreCollection.create_index ([("user_id", 1), ("store_id", 1)], unique=True)
 
     def add_book(self, user_id: str, store_id: str, book_id: str, book_json_str: str, stock_level: int):
         try:
-            if not self.user_id_exist(user_id):
+            if not user_id_exist(user_id):
                 return error.error_non_exist_user_id(user_id)
-            if not self.store_id_exist(store_id):
+            if not store_id_exist(store_id):
                 return error.error_non_exist_store_id(store_id)
-            if self.book_id_exist(store_id, book_id):
+            if book_id_exist(store_id, book_id):
                 return error.error_exist_book_id(book_id)
 
-            self.conn.execute("INSERT into store(store_id, book_id, book_info, stock_level)"
-                              "VALUES (?, ?, ?, ?)", (store_id, book_id, book_json_str, stock_level))
-            self.conn.commit()
-        except sqlite.Error as e:
+            # self.conn.execute("INSERT into store(store_id, book_id, book_info, stock_level)"
+            #                   "VALUES (?, ?, ?, ?)", (store_id, book_id, book_json_str, stock_level))
+            # self.conn.commit()
+
+            self.storeCollection.insert_one (
+                {
+                    "store_id": store_id, 
+                    "book_id": book_id, 
+                    "book_info": book_json_str, 
+                    "stock_level": stock_level
+                }
+            )
+        except pymongo.errors.PyMongoError as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
@@ -28,32 +45,42 @@ class Seller(db_conn.DBConn):
 
     def add_stock_level(self, user_id: str, store_id: str, book_id: str, add_stock_level: int):
         try:
-            if not self.user_id_exist(user_id):
+            if not user_id_exist(user_id):
                 return error.error_non_exist_user_id(user_id)
-            if not self.store_id_exist(store_id):
+            if not store_id_exist(store_id):
                 return error.error_non_exist_store_id(store_id)
-            if not self.book_id_exist(store_id, book_id):
+            if not book_id_exist(store_id, book_id):
                 return error.error_non_exist_book_id(book_id)
 
-            self.conn.execute("UPDATE store SET stock_level = stock_level + ? "
-                              "WHERE store_id = ? AND book_id = ?", (add_stock_level, store_id, book_id))
-            self.conn.commit()
-        except sqlite.Error as e:
+            # self.conn.execute("UPDATE store SET stock_level = stock_level + ? "
+            #                   "WHERE store_id = ? AND book_id = ?", (add_stock_level, store_id, book_id))
+            # self.conn.commit()
+            update_result = self.storeCollection.update_one (
+                {"store_id": store_id, "book_id": book_id},
+                {"$inc": {"stock_level": add_stock_level}}
+            )
+        except pymongo.errors.PyMongoError as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
         return 200, "ok"
 
-    def create_store(self, user_id: str, store_id: str) -> (int, str):
+    def create_store(self, user_id: str, store_id: str) -> Tuple[int, str]:
         try:
-            if not self.user_id_exist(user_id):
+            if not user_id_exist(user_id):
                 return error.error_non_exist_user_id(user_id)
-            if self.store_id_exist(store_id):
+            if store_id_exist(store_id):
                 return error.error_exist_store_id(store_id)
-            self.conn.execute("INSERT into user_store(store_id, user_id)"
-                              "VALUES (?, ?)", (store_id, user_id))
-            self.conn.commit()
-        except sqlite.Error as e:
+            # self.conn.execute("INSERT into user_store(store_id, user_id)"
+            #                   "VALUES (?, ?)", (store_id, user_id))
+            # self.conn.commit()
+            self.userStoreCollection.insert_one (
+                {
+                    "store_id": store_id, 
+                    "user_id": user_id
+                }
+            )
+        except pymongo.errors.PyMongoError as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
